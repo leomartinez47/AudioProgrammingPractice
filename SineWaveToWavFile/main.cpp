ec4cdca264b7751ce2fd9ed0f1a084426e1be865
leomartinez47 <leomartinez4723@gmail.com>
2025-10-26 00:43:09 +0000

    Wrote some sounds for sound effects I need for a class project.

--- SineWaveToWavFile/main.cpp
+++ SineWaveToWavFile/main.cpp
@@ -7,7 +7,7 @@ using std::endl;
 /// @brief Creates a Sine wav to represent a 10 second A 440 note and writes the
 ///        wave data to a wav file.
 int main(int argc, char* argv[]) {
-  Sine sineWave(15000, 440, 44100, 10);
+  Sine sineWave(15000, 660, 44100, 1);
   Wav wavFile(1);
   int dataSize = sineWave.getTotalSamples();
   double waveData[dataSize];

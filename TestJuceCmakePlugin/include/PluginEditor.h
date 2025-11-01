8df4cb34589a8c8758f4f5da5bc4af1018da6798
leomartinez47 <leomartinez4723@gmail.com>
2025-11-01 17:19:30 -0600

    Testing JUCE with Cmake.
    It's time to start making some vst's, and The Audio Programmer on YouTube seems to have some good resources for working with the JUCE framework with Cmake. So TestJuceCmakePlugin is me getting a JUCE plugin project to build and run using Cmake, and it worked pretty well.
    
    Note: JUCE is not compatible with MinGW so remember to use the MSVS toolchain.

---
+++ TestJuceCmakePlugin/include/PluginEditor.h
@@ -0,0 +1,33 @@
+﻿//
+// Created by Leo Martinez on 11/1/2025.
+// Code from The Audio Programmer on youtube
+// https://www.youtube.com/watch?v=WZCX-RmJN1s&t=1201s
+//
+
+#ifndef AUDIOPROGRAMMINGPRACTICE_PLUGINEDITOR_H
+#define AUDIOPROGRAMMINGPRACTICE_PLUGINEDITOR_H
+
+#pragma once
+
+#include "PluginProcessor.h"
+
+//==============================================================================
+class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor
+{
+public:
+  explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
+  ~AudioPluginAudioProcessorEditor() override;
+
+  //==============================================================================
+  void paint (juce::Graphics&) override;
+  void resized() override;
+
+private:
+  // This reference is provided as a quick way for your editor to
+  // access the processor object that created it.
+  AudioPluginAudioProcessor& processorRef;
+
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
+};
+
+#endif //AUDIOPROGRAMMINGPRACTICE_PLUGINEDITOR_H
---
+++ TestJuceCmakePlugin/src/PluginEditor.cpp
@@ -0,0 +1,39 @@
+﻿//
+// Created by Leo Martinez on 11/1/2025.
+// Code from The Audio Programmer on youtube
+// https://www.youtube.com/watch?v=WZCX-RmJN1s&t=1201s
+//
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
+    : AudioProcessorEditor (&p), processorRef (p)
+{
+  juce::ignoreUnused (processorRef);
+  // Make sure that before the constructor has finished, you've set the
+  // editor's size to whatever you need it to be.
+  setSize (400, 300);
+}
+
+AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
+{
+  // (Our component is opaque, so we must completely fill the background with a solid colour)
+  g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+  g.setColour (juce::Colours::white);
+  g.setFont (15.0f);
+  g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void AudioPluginAudioProcessorEditor::resized()
+{
+  // This is generally where you'll want to lay out the positions of any
+  // subcomponents in your editor..
+}
